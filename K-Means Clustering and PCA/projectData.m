229fec81912883b4d68b8e6ac306d08e467547ee
hridayns <hridaysanghvi@gmail.com>
2017-12-24 10:49:26 +0530

    update projectData.m - Project Data (PCA)

--- K-Means Clustering and PCA/projectData.m
+++ K-Means Clustering and PCA/projectData.m
@@ -9,6 +9,8 @@ function Z = projectData(X, U, K)
 % You need to return the following variables correctly.
 Z = zeros(size(X, 1), K);
 
+U_reduce = U(:,1:K);
+m = size(X,1);
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the projection of the data using only the top K 
 %               eigenvectors in U (first K columns). 
@@ -18,8 +20,7 @@ Z = zeros(size(X, 1), K);
 %                    projection_k = x' * U(:, k);
 %
 
-
-
+Z = X * U_reduce;
 
 % =============================================================
 
