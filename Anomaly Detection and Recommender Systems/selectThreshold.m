3e3c7b31c7865f5b3f8ac62fc3040e8d03962c49
hridayns <hridaysanghvi@gmail.com>
2017-12-26 10:31:43 +0530

    update selectThreshold.m - Select Threshold

--- Anomaly Detection and Recommender Systems/selectThreshold.m
+++ Anomaly Detection and Recommender Systems/selectThreshold.m
@@ -25,16 +25,17 @@ for epsilon = min(pval):stepsize:max(pval)
 
 
 
-
-
-
-
-
-
-
-
-
-
+    predictions = (pval < epsilon);
+    
+    tp = sum( (predictions == 1) & (yval == 1) );
+    fp = sum( (predictions == 1) & (yval == 0) );
+    fn = sum( (predictions == 0) & (yval == 1) );
+    
+    prec = tp/(tp + fp);
+    rec = tp/(tp + fn);
+    
+    F1 = (2*prec*rec) / (prec + rec);
+    
     % =============================================================
 
     if F1 > bestF1
